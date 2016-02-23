6b9a7c1ab5f355f8559c7364829b131e055222b1
Breiden Busch <breiden.busch@gmail.com>
2016-02-22 18:55:36 -0700

    Finished Project

--- js/app.js
+++ js/app.js
@@ -6,4 +6,42 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     // routing configuration code
 
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'homeCtrl',
+      views: {
+        'utahjazz': {
+          templateUrl: 'js/teams/teamTmpl.html',
+          controller: 'teamCtrl',
+          resolve: {teamData:
+             function(teamService) {return teamService.getTeamData("utahjazz");}}
+        },
+        'losangeleslakers': {
+          templateUrl: 'js/teams/teamTmpl.html',
+          controller: 'teamCtrl',
+          resolve: {teamData:
+             function(teamService) {return teamService.getTeamData("losangeleslakers");}}
+        },
+        'miamiheat': {
+          templateUrl: 'js/teams/teamTmpl.html',
+          controller: 'teamCtrl',
+          resolve: {teamData:
+             function(teamService) {return teamService.getTeamData("miamiheat");}}
+        }
+
+      }
+    })
+    .state('teams', {
+      url: '/teams/:team',
+      templateUrl: 'js/teams/teamTmpl.html',
+      controller: 'teamCtrl',
+      resolve: {teamData:
+         function(teamService, $stateParams) {return teamService.getTeamData($stateParams.team);}}
+    });
+
+    $urlRouterProvider
+    .otherwise('/');
+
 });
